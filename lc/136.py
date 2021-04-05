0a48deecea900f4ae2bc22ced9b3d3e345ba963e
Haru <jbh922@gmail.com>
2021-04-05 20:31:54 +0900

    2021-04-05

--- lc/136.py
+++ lc/136.py
@@ -1,9 +1,8 @@
+from collections import defaultdict
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        num_counts={}
+        num_counts=defaultdict(int)
         for num in nums:
-            if num not in num_counts:
-                num_counts[num]=0
             num_counts[num]+=1
         for num in num_counts:
             if num_counts[num]==1:
